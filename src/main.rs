2df58b56ec2e35079696f7958323328bab8728a7
wumpini.hussein <wumpini.hussein@gmail.com>
2022-01-03 15:51:33 +0000

    add POST endpoint to generate new url

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,24 @@
 extern crate rocket;
 
 use dashmap::DashMap;
+use rand::{Rng, thread_rng};
+use rocket::response::status::BadRequest;
+use rocket::State;
 
 #[get("/")]
 fn index() -> &'static str {
     "Hello world!"
+}
+
+#[post("/api/shorten?<url>")]
+fn shorten(url: String, state: &State<DashMap<u32, String>>) -> Result<String, BadRequest<&str>> {
+    if url.is_empty() {
+        Err(BadRequest(Some("URL is empty!")))
+    } else {
+        let key: u32 = thread_rng.gen();
+        state.insert(key, url);
+        Ok(key.to_string())
+    }
 }
 
 #[launch]

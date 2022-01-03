6a6abfa59b81906f4a52c9b51d66c377f055e6d3
wumpini.hussein <wumpini.hussein@gmail.com>
2022-01-03 16:12:12 +0000

    add GET endpoint for redirecting to generated URLs

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,8 @@ extern crate rocket;
 
 use dashmap::DashMap;
 use rand::{Rng, thread_rng};
-use rocket::response::status::BadRequest;
+use rocket::response::Redirect;
+use rocket::response::status::{BadRequest, NotFound};
 use rocket::State;
 
 #[get("/")]
@@ -11,12 +12,20 @@ fn index() -> &'static str {
     "Hello world!"
 }
 
+#[get("/<key>")]
+fn redirect(key: u32, state: &State<DashMap<u32, String>>) -> Result<Redirect, NotFound<&str>> {
+    state
+        .get(&key)
+        .map(|url| Redirect::to(url.clone()))
+        .ok_or(NotFound("Invalid or expired link!"))
+}
+
 #[post("/api/shorten?<url>")]
 fn shorten(url: String, state: &State<DashMap<u32, String>>) -> Result<String, BadRequest<&str>> {
     if url.is_empty() {
         Err(BadRequest(Some("URL is empty!")))
     } else {
-        let key: u32 = thread_rng.gen();
+        let key: u32 = thread_rng().gen();
         state.insert(key, url);
         Ok(key.to_string())
     }
@@ -26,5 +35,5 @@ fn shorten(url: String, state: &State<DashMap<u32, String>>) -> Result<String, B
 fn rocket() -> _ {
     rocket::build()
         .manage(DashMap::<u32, String>::new()) // tell rocket to manage the state of this concurrent hashmap
-        .mount("/", routes![index])
+        .mount("/", routes![index, shorten, redirect])
 }
